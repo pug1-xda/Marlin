d5cb657cb8c175aadd4c85446481af6f59d60f84
Rob Johnson <skunkey_monkey@hotmail.co.uk>
2019-03-18 10:40:23 +0000

    fix conflicts

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1211,12 +1211,12 @@
  * Override if the automatically selected points are inadequate.
  */
 #if EITHER(AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_UBL)
-  //#define PROBE_PT_1_X 15
-  //#define PROBE_PT_1_Y 180
-  //#define PROBE_PT_2_X 15
-  //#define PROBE_PT_2_Y 20
-  //#define PROBE_PT_3_X 170
-  //#define PROBE_PT_3_Y 20
+  #define PROBE_PT_1_X 115
+  #define PROBE_PT_1_Y 115
+  #define PROBE_PT_2_X 125
+  #define PROBE_PT_2_Y 125
+  #define PROBE_PT_3_X 135
+  #define PROBE_PT_3_Y 135
 #endif
 
 /**
