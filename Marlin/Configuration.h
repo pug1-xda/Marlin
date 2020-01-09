0504583462e7e3d7fe5b88289d06468b4891f77e
pug1-xda <skunkey_monkey@hotmail.co.uk>
2020-01-09 16:05:07 +0000

    BL touch clone
    These are the angls of the srevo, 50, holds the flag up while printing and 0 loweres it for leveling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -865,7 +865,7 @@
  * Z Servo Probe, such as an endstop switch on a rotating arm.
  */
 #define Z_PROBE_SERVO_NR 0       // Defaults to SERVO 0 connector.
-#define Z_SERVO_ANGLES { 5, 180 } // Z Servo Deploy and Stow angles
+#define Z_SERVO_ANGLES { 50, 0 } // Z Servo Deploy and Stow angles
 
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
