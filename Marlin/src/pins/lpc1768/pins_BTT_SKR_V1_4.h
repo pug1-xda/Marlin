2e09ada4151518b072c669d40ea1bf096478794d
pug1-xda <skunkey_monkey@hotmail.co.uk>
2020-12-08 21:20:59 +0000

    ps_on pin, remove filament 2 sensor

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
@@ -99,13 +99,13 @@
 // Filament Runout Sensor
 //
 #define FIL_RUNOUT_PIN                     P1_26  // E0DET
-#define FIL_RUNOUT2_PIN                    P1_25  // E1DET
+//#define FIL_RUNOUT2_PIN                    P1_25  // E1DET
 
 //
 // Power Supply Control
 //
 #ifndef PS_ON_PIN
-  #define PS_ON_PIN                        P1_00  // PWRDET
+  #define PS_ON_PIN                        P1_25  // PWRDET
 #endif
 
 //
