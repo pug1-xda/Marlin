b1260828c2f35d73f3098555576b6c0176a9f3cb
pug1-xda <skunkey_monkey@hotmail.co.uk>
2021-03-14 18:27:46 +0000

    retractions

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1995,13 +1995,13 @@
     #define MIN_AUTORETRACT 0.1           // (mm) Don't convert E moves under this length
     #define MAX_AUTORETRACT 10.0          // (mm) Don't convert E moves over this length
   #endif
-  #define RETRACT_LENGTH 5.25             // (mm) Default retract length (positive value)
+  #define RETRACT_LENGTH 6.5             // (mm) Default retract length (positive value)
   #define RETRACT_LENGTH_SWAP 13          // (mm) Default swap retract length (positive value)
-  #define RETRACT_FEEDRATE 65             // (mm/s) Default feedrate for retracting
+  #define RETRACT_FEEDRATE 80             // (mm/s) Default feedrate for retracting
   #define RETRACT_ZRAISE 0.65             // (mm) Default retract Z-raise
-  #define RETRACT_RECOVER_LENGTH 0.15     // (mm) Default additional recover length (added to retract length on recover)
+  #define RETRACT_RECOVER_LENGTH -0.05     // (mm) Default additional recover length (added to retract length on recover)
   #define RETRACT_RECOVER_LENGTH_SWAP 2   // (mm) Default additional swap recover length (added to retract length on recover from toolchange)
-  #define RETRACT_RECOVER_FEEDRATE 65    // (mm/s) Default feedrate for recovering from retraction
+  #define RETRACT_RECOVER_FEEDRATE 80    // (mm/s) Default feedrate for recovering from retraction
   #define RETRACT_RECOVER_FEEDRATE_SWAP 15 // (mm/s) Default feedrate for recovering from swap retraction
   #if ENABLED(MIXING_EXTRUDER)
     //#define RETRACT_SYNC_MIXING         // Retract and restore all mixing steppers simultaneously
